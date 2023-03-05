f311be33edbe4a5a260f8a75198e2c9488ee0207
zhiriezra <zhiriezra@gmail.com>
2023-03-05 23:51:04 +0100

    admin layout

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class BusinessController extends Controller
 {
-    //
+    public function create()
+    {
+        # code...
+    }
+
+    public function store()
+    {
+        # code...
+    }
+
+    public function edit()
+    {
+        # code...
+    }
+
+    public function update()
+    {
+        # code...
+    }
+
+    public function destroy()
+    {
+        # code...
+    }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    
+    public function create()
+    {
+        # code...
+    }
+
+    public function store()
+    {
+        # code...
+    }
+
+    public function edit()
+    {
+        # code...
+    }
+
+    public function update()
+    {
+        # code...
+    }
+
+    public function destroy()
+    {
+        # code...
+    }
 }
