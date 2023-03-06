5ede28b01e71c8003d363afc0606be84774e9b95
zhiriezra <zhiriezra@gmail.com>
2023-03-06 01:14:26 +0100

    business and product views updated

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Business;
+use App\Models\Business;
+use Illuminate\Support\Str;
+use Image;
 
 class BusinessController extends Controller
 {
